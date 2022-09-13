5a467fad30e1e98cb309aafdf7a90b4f048e67e6
REevee0 <79875968+REevee0@users.noreply.github.com>
2022-09-14 00:18:22 +0300

    Update BME280.h

--- BME280.h
+++ BME280.h
@@ -1,24 +1,3 @@
-/*
- ***************************************************************************************************************
- ***************************************************************************************************************
- ***************************************************************************************************************
-
-  File:		  BME280_STM32.h
-  Author:     ControllersTech.com
-  Updated:    Dec 14, 2021
-
- ***************************************************************************************************************
-  Copyright (C) 2017 ControllersTech.com
-
-  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
-  of the GNU General Public License version 3 as published by the Free Software Foundation.
-  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-  or indirectly by this software, read more about this on the GNU General Public License.
-
- ***************************************************************************************************************
- */
-
-
 #ifndef INC_BME280_H_
 #define INC_BME280_H_
 
